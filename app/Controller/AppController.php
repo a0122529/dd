47831427a86e38544a59d7afeb94aa5ee29dc791
Ashok Vishwakarma <akvlko@gmail.com>
2015-01-09 23:20:46 +0530

    Ïnitial Commit

---
+++ app/Controller/AppController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Application level Controller
+ *
+ * This file is application-wide controller file. You can put all
+ * application-wide controller-related methods here.
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Controller
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+App::uses('Controller', 'Controller');
+
+/**
+ * Application Controller
+ *
+ * Add your application-wide methods in the class below, your controllers
+ * will inherit them.
+ *
+ * @package		app.Controller
+ * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+ */
+class AppController extends Controller {
+	public $config;
+	public function beforeFilter(){
+		$this->config = Configure::read('config');
+		$this->set('config', $this->config);
+		$this->set('title_for_layout', 'Home');
+		$this->set('meta_description', $this->config['default_meta_description']);
+		$this->set('meta_keywords', $this->config['default_meta_keywords']);
+		/*if(isset($this->request->params['admin'])){
+			// an admin prefix call is being made
+		}*/
+	}
+}
---
+++ app/View/Layouts/default-bkp.ctp
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta content="<?php echo $title_for_layout;?>" lang="en" name="title" />
+		<meta content="<?php echo $meta_description;?>" lang="en" name="description" />
+		<meta content="<?php echo $meta_keywords;?>" lang="en" name="keywords" />
+		<meta content="daydeeds" lang="en" name="author" />
+		<title><?php echo $title_for_layout; ?> <?php echo $config['base_title'];?></title>
+		<?php echo $this->Html->meta('icon', 'favicon.png', array('type'=>'image/png'));?>
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,900|Open+Sans:300,400,600,700,900' rel='stylesheet' type='text/css'>
+		<?php echo $this->Html->css('lib/animate'); ?>
+		<?php echo $this->Html->css('lib/bootstrap.min'); ?>
+		<?php echo $this->Html->css('lib/fa.min'); ?>
+		<?php echo $this->Html->css('daydeeds'); ?>
+	</head>
+</html>
+<body class="white-bg">
+<div id="header"><?php echo $this->element('header');?></div>
+<div id="page"><?php echo $this->fetch('content'); ?></div>
+<div id="footer"><?php echo $this->element('footer');?></div>
+</body>
+</html>
